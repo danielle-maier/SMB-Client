de3c1e6caec1cacee8f5961a5372d97cbe036bb9
Danielle Maier <damaier@gmail.com>
2017-07-27 17:00:27 -0600

    😋 here comes the style

--- scripts/app.js
+++ scripts/app.js
@@ -5,20 +5,33 @@ $(document).ready(function() {
     .then((messageData) => {
       let posts = messageData;
       let messages = document.getElementById("messages");
-      messages.innerHTML = displayPost(messageData);
+      messages.innerHTML = displayAllPosts(messageData);
     });
 });
 
-function displayPost(data) {
+function displayPost(data,postId){
+  return `<div class="card blue-grey lighten-4">
+            <div class="card-content black-text">
+              <span class="card-title"><i class="small material-icons">chat</i>   ${data[postId].id}</span>
+              <h3>${data[postId].title}</h3>
+              <p>${data[postId].messageBody}</p>
+              <p>By: ${data[postId].name}</p>
+              <p>Posted: ${data[postId].updatedAt}</p>
+            </div>
+          </div>`
+}
+
+function displayAllPosts(data) {
   let htmlCardArray = [];
   for (i = 0; i < data.length; i++) {
     htmlCardArray[i] = `<div class="card blue-grey lighten-4">
-                        <div class="card-content black-text">
-                        <span class="card-title"><i class="small material-icons">announcement</i>  ${data[i].title}</span>
-                        <p>${data[i].messageBody}</p>
-                        <p>By: ${data[i].name}</p>
-                        <p>Posted: ${data[i].updatedAt}</p>
-                        </div>
+                          <div class="card-content black-text">
+                            <span class="card-title"><i class="small material-icons">chat</i>   ${data[i].id}</span>
+                            <h3>${data[i].title}</h3>
+                            <p>${data[i].messageBody}</p>
+                            <p>By: ${data[i].name}</p>
+                            <p>Posted: ${data[i].updatedAt}</p>
+                          </div>
                         </div>`
   }
   return htmlCardArray;
